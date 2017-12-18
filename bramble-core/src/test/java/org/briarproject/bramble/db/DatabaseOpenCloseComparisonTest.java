1ccfea9cb28e73061642afbd5ea8bc2aca3523e0
akwizgran <michael@briarproject.org>
2017-12-18 15:10:09 +0000

    Compare open/run/close durations and DB sizes.

--- bramble-core/src/test/java/org/briarproject/bramble/db/DatabaseOpenCloseComparisonTest.java
+++ bramble-core/src/test/java/org/briarproject/bramble/db/DatabaseOpenCloseComparisonTest.java
@@ -5,17 +5,24 @@ import org.briarproject.bramble.api.db.DbException;
 import org.briarproject.bramble.api.system.Clock;
 import org.briarproject.bramble.system.SystemClock;
 import org.briarproject.bramble.test.TestDatabaseConfig;
+import org.briarproject.bramble.test.UTest;
 
 import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.briarproject.bramble.test.TestUtils.deleteTestDirectory;
+import static org.briarproject.bramble.test.TestUtils.getMean;
+import static org.briarproject.bramble.test.TestUtils.getMedian;
+import static org.briarproject.bramble.test.TestUtils.getStandardDeviation;
+import static org.briarproject.bramble.test.UTest.Z_CRITICAL_0_01;
 
 public abstract class DatabaseOpenCloseComparisonTest
 		extends DatabasePerformanceTest {
 
-	private static final int RUNS = 30;
+	private static final int RUNS = 100;
 
 	abstract Database<Connection> createDatabase(boolean conditionA,
 			DatabaseConfig databaseConfig, Clock clock);
@@ -23,15 +30,36 @@ public abstract class DatabaseOpenCloseComparisonTest
 	@Override
 	protected void benchmark(String name,
 			BenchmarkTask<Database<Connection>> task) throws Exception {
-		benchmark(true, name, task);
-		benchmark(false, name, task);
+		List<BenchmarkResult> aResults, bResults;
+		boolean aFirst = random.nextBoolean();
+		if (aFirst) {
+			aResults = benchmark(true, name, task);
+			bResults = benchmark(false, name, task);
+		} else {
+			bResults = benchmark(false, name, task);
+			aResults = benchmark(true, name, task);
+		}
+		writeResult(name, "open", getOpenDurations(aResults),
+				getOpenDurations(bResults));
+		writeResult(name, "run", getRunDurations(aResults),
+				getRunDurations(bResults));
+		writeResult(name, "close", getCloseDurations(aResults),
+				getCloseDurations(bResults));
+		writeResult(name, "size", getSizes(aResults), getSizes(bResults));
 	}
 
-	private void benchmark(boolean conditionA, String name,
+	private List<BenchmarkResult> benchmark(boolean conditionA, String name,
 			BenchmarkTask<Database<Connection>> task) throws Exception {
 		createDatabase(conditionA);
-		for (int i = 0; i < RUNS; i++)
-			writeResult(name, conditionA, benchmark(conditionA, task));
+		List<BenchmarkResult> results = new ArrayList<>(RUNS);
+		for (int i = 0; i < RUNS; i++) {
+			BenchmarkResult result = benchmark(conditionA, task);
+			System.out.println(String.format("%s\t%s\t%,d\t%,d\t%,d\t%,d",
+					name, conditionA, result.open, result.run, result.close,
+					result.size));
+			results.add(result);
+		}
+		return results;
 	}
 
 	private void createDatabase(boolean conditionA) throws Exception {
@@ -89,10 +117,40 @@ public abstract class DatabaseOpenCloseComparisonTest
 		return 0;
 	}
 
-	private void writeResult(String name, boolean conditionA,
-			BenchmarkResult res) throws IOException {
-		String result = String.format("%s\t%s\t%,d\t%,d\t%,d\t%,d", name,
-				conditionA, res.open, res.run, res.close, res.size);
+	private List<Double> getOpenDurations(List<BenchmarkResult> results) {
+		List<Double> durations = new ArrayList<>(results.size());
+		for (BenchmarkResult r : results) durations.add((double) r.open);
+		return durations;
+	}
+
+	private List<Double> getRunDurations(List<BenchmarkResult> results) {
+		List<Double> durations = new ArrayList<>(results.size());
+		for (BenchmarkResult r : results) durations.add((double) r.run);
+		return durations;
+	}
+
+	private List<Double> getCloseDurations(List<BenchmarkResult> results) {
+		List<Double> durations = new ArrayList<>(results.size());
+		for (BenchmarkResult r : results) durations.add((double) r.close);
+		return durations;
+	}
+
+	private List<Double> getSizes(List<BenchmarkResult> results) {
+		List<Double> durations = new ArrayList<>(results.size());
+		for (BenchmarkResult r : results) durations.add((double) r.size);
+		return durations;
+	}
+
+	private void writeResult(String name, String stage, List<Double> aSamples,
+			List<Double> bSamples) throws IOException {
+		UTest.Result comparison =
+				UTest.test(aSamples, bSamples, Z_CRITICAL_0_01);
+		String result = String.format(
+				"%s\t%s\t%,d\t%,d\t%,d\t%,d\t%,d\t%,d\t%s", name, stage,
+				(long) getMean(aSamples), (long) getMedian(aSamples),
+				(long) getStandardDeviation(aSamples),
+				(long) getMean(bSamples), (long) getMedian(bSamples),
+				(long) getStandardDeviation(bSamples), comparison);
 		writeResult(result);
 	}
 
