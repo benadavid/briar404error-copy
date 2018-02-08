cd49bd0351f19cb8b0d738f2c7e48b8cea8b5df5
benadavid <benadavid_2007@hotmail.com>
2018-02-08 18:41:59 -0500

    add permission

--- briar-android/src/main/java/org/briarproject/briar/android/navdrawer/Constants.java
+++ briar-android/src/main/java/org/briarproject/briar/android/navdrawer/Constants.java
@@ -4,6 +4,6 @@ package org.briarproject.briar.android.navdrawer;
  * Created by David on 2018-02-08.
  */
 public class Constants {
-	public static final String UPLOAD_URL = "http://192.168.0.108/AndroidUploadImage/upload.php";
-	public static final String IMAGES_URL = "http://192.168.0.108/AndroidUploadImage/getImages.php";
+	public static final String UPLOAD_URL = "http://192.168.0.102/AndroidUploadImage/upload.php";
+	public static final String IMAGES_URL = "http://192.168.0.102/AndroidUploadImage/getImages.php";
 }
