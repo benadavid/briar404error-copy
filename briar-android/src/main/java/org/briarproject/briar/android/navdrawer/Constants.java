d8bbc5e65eec300f193d9d6946477d90026997b0
David Benalal <benadavid_2007@hotmail.com>
2018-02-08 15:15:26 -0500

    create constants class

---
+++ briar-android/src/main/java/org/briarproject/briar/android/navdrawer/Constants.java
@@ -0,0 +1,9 @@
+package org.briarproject.briar.android.navdrawer;
+
+/**
+ * Created by David on 2018-02-08.
+ */
+public class Constants {
+	public static final String UPLOAD_URL = "http://192.168.0.108/AndroidUploadImage/upload.php";
+	public static final String IMAGES_URL = "http://192.168.0.108/AndroidUploadImage/getImages.php";
+}
