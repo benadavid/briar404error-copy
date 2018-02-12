f466fa9cc1884cc093c6da00783594846d322410
Gibran Khan <jbk5988@gmail.com>
2018-02-11 19:13:29 -0500

    #9 Added dummy test

--- briar-android/src/test/java/org/briarproject/briar/android/navdrawer/NavDrawerActivityTest.java
+++ briar-android/src/test/java/org/briarproject/briar/android/navdrawer/NavDrawerActivityTest.java
@@ -1,8 +1,40 @@
+package org.briarproject.briar.android.navdrawer;
+
+import android.content.Intent;
+
+import org.briarproject.bramble.test.TestUtils;
+import org.briarproject.briar.android.TestBriarApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Gibran on 2018-02-11.
  */
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = 21, application = TestBriarApplication.class,
+		packageName = "org.briarproject.briar")
 public class NavDrawerActivityTest {
 
+	private TestNavDrawerActivity navDrawerActivity;
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		Intent intent = new Intent();
+		intent.putExtra("briar.GROUP_ID", TestUtils.getRandomId());
+		navDrawerActivity = Robolectric.buildActivity(TestNavDrawerActivity.class,
+				intent).create().start().resume().get();
+	}
+
+	@Test
+	public void stuff(){
+		System.out.println("Meow");
+	}
 }
--- briar-android/src/test/java/org/briarproject/briar/android/navdrawer/TestNavDrawerActivity.java
+++ briar-android/src/test/java/org/briarproject/briar/android/navdrawer/TestNavDrawerActivity.java
@@ -1,8 +1,45 @@
 package org.briarproject.briar.android.navdrawer;
 
+import android.os.Bundle;
+
+import org.briarproject.bramble.api.nullsafety.MethodsNotNullByDefault;
+import org.briarproject.bramble.api.nullsafety.ParametersNotNullByDefault;
+import org.briarproject.briar.R;
+import org.briarproject.briar.android.activity.ActivityModule;
+import org.briarproject.briar.android.activity.BaseActivity;
+import org.briarproject.briar.android.controller.BriarController;
+import org.briarproject.briar.android.controller.BriarControllerImpl;
+import org.briarproject.briar.android.threaded.ThreadItemAdapter;
+import org.mockito.Mockito;
+
+import javax.annotation.Nullable;
+
 /**
  * Created by Gibran on 2018-02-11.
  */
+@MethodsNotNullByDefault
+@ParametersNotNullByDefault
+public class TestNavDrawerActivity extends NavDrawerActivity{
 
-public class TestNavDrawerActivity {
+	@Override
+	public void onCreate(@Nullable Bundle state) {
+		setTheme(R.style.BriarTheme_NoActionBar);
+		super.onCreate(state);
+	}
+
+	@Override
+	protected ActivityModule getActivityModule() {
+		return new ActivityModule(this) {
+
+			@Override
+			protected BriarController provideBriarController(
+					BriarControllerImpl briarController) {
+				BriarController c = Mockito.mock(BriarController.class);
+				Mockito.when(c.hasEncryptionKey()).thenReturn(true);
+				return c;
+			}
+
+		};
+	}
 }
+
