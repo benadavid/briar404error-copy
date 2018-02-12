7623f20dd0d4a6819efcfb7c014c9dd887c0022a
Gibran Khan <jbk5988@gmail.com>
2018-02-11 19:11:17 -0500

    #9 Added unit testing base configuration

---
+++ briar-android/src/test/java/org/briarproject/briar/android/navdrawer/NavDrawerActivityTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Gibran on 2018-02-11.
+ */
+public class NavDrawerActivityTest {
+
+}
---
+++ briar-android/src/test/java/org/briarproject/briar/android/navdrawer/TestNavDrawerActivity.java
@@ -0,0 +1,8 @@
+package org.briarproject.briar.android.navdrawer;
+
+/**
+ * Created by Gibran on 2018-02-11.
+ */
+
+public class TestNavDrawerActivity {
+}
