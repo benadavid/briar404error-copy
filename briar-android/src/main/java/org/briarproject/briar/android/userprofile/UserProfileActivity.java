9013fd88a6cc6f1585266cfa26ac95a5edcbaa42
rajee10 <rvairam10@gmail.com>
2018-03-15 17:06:36 -0400

    Implements changing the user's avatar by clicking the avatar image using an existing implementation of subtask #9. An edit image was added ontop of the avatar to let users know this field is editable. To ensure consistency, onStart and onCreate checks if an image exists in the internal memory so that users will be able to see the changes to the avatar right away.
    Subtask #55

--- briar-android/src/main/java/org/briarproject/briar/android/userprofile/UserProfileActivity.java
+++ briar-android/src/main/java/org/briarproject/briar/android/userprofile/UserProfileActivity.java
@@ -1,39 +1,102 @@
 package org.briarproject.briar.android.userprofile;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Environment;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import org.briarproject.briar.R;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 public class UserProfileActivity extends AppCompatActivity {
+
+	private StorageReference mStorageRef; //Firebase storage object
+	private ImageButton avatarButton;
+	private static final String LOG_TAG = UserProfileActivity.class.getSimpleName();
+	public final static String APP_PATH_SD_CARD="/DesiredSubfolderName/";
+	public final static String APP_THUMBNAIL_PATH_SD_CARD="thumbnails";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_profile);
 
+		//Firebase storage object initalized
+		mStorageRef = FirebaseStorage.getInstance().getReference();
 
-
-
-		/*
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
-
-		FloatingActionButton fab =
-				(FloatingActionButton) findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
+		avatarButton = findViewById(R.id.AvatarButton);
+		avatarButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action",
-						Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
+			startActivity(new Intent (UserProfileActivity.this,
+					AvatarActivity.class));
 			}
 		});
-		*/
+
+		// load pic from internal memory and format it into a circle
+		if(fileExistance("desiredFilename.png")) {
+			// Make the image into a circle
+			// In saveImageToInternalStorage() we named the picture desiredFilename
+			RoundedBitmapDrawable roundedBitmapDrawable= RoundedBitmapDrawableFactory
+					.create(getResources(), getThumbnail("desiredFilename.png"));
+			roundedBitmapDrawable.setCircular(true);
+			avatarButton.setImageDrawable(roundedBitmapDrawable);
+		}
+	}
+
+	public void onStart() {
+		super.onStart();
+
+		//load pic from internal memory and format it into a circle
+		if(fileExistance("desiredFilename.png")) {
+			// Make the image into a circle
+			// In saveImageToInternalStorage() we named the picture desiredFilename
+			RoundedBitmapDrawable roundedBitmapDrawable =
+					RoundedBitmapDrawableFactory.create(getResources(),
+							getThumbnail("desiredFilename.png"));
+			roundedBitmapDrawable.setCircular(true);
+			avatarButton.setImageDrawable(roundedBitmapDrawable);
+		}
+
+
+	}
+
+	//check if the picture exists in the internal memory before trying to load
+	// the picture to prevent crash
+	public boolean fileExistance(String fname){
+		File file = getBaseContext().getFileStreamPath(fname);
+		return file.exists();
+	}
+
+	public Bitmap getThumbnail(String filename) {
+
+		String fullPath = Environment.getExternalStorageDirectory().getAbsolutePath()
+				+ APP_PATH_SD_CARD + APP_THUMBNAIL_PATH_SD_CARD;
+		Bitmap thumbnail = null;
+
+
+		// If no file on external storage, look in internal storage
+		if (thumbnail == null) {
+			try {
+				File filePath = getFileStreamPath(filename);
+				FileInputStream fi = new FileInputStream(filePath);
+				thumbnail = BitmapFactory.decodeStream(fi);
+			} catch (Exception ex) {
+				Log.e(LOG_TAG + "getThumbnail() failed", ex.getMessage());
+			}
+		}
+		return thumbnail;
 	}
 
 }
