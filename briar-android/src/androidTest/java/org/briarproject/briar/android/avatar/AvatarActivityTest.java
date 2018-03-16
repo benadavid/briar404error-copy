fee73c9d27745304ec3c8fefd1d6330fe1ee1a2c
benadavid <benadavid_2007@hotmail.com>
2018-03-15 22:58:31 -0400

    androidTest has been added to the project. src/test is for pure unit testing while src/androidTest is needed for unit tests that involve android instrumentation.

---
+++ briar-android/src/androidTest/java/org/briarproject/briar/android/avatar/AvatarActivityTest.java
@@ -0,0 +1,17 @@
+package org.briarproject.briar.android.avatar;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by David on 2018-03-15.
+ */
+public class AvatarActivityTest {
+	@org.junit.Before
+	public void setUp() throws Exception {
+	}
+
+	@org.junit.After
+	public void tearDown() throws Exception {
+	}
+
+}
