295e5b0fd5eca94105363275ccc9f4c5e41b28ce
Zackkogan <zackkogan@hotmail.com>
2018-03-18 18:52:45 -0400

    Implement Markdown
    Resolves #31
    A new library was recently discovered that does not conflict with current dependencies. This library requires no changes to the UI, metadata, database or any refactoring of the code to function, essentially rendering all commits to this issue prior to this one irrelevant.

--- briar-android/src/main/java/org/briarproject/briar/android/contact/ConversationItemViewHolder.java
+++ briar-android/src/main/java/org/briarproject/briar/android/contact/ConversationItemViewHolder.java
@@ -3,9 +3,12 @@ package org.briarproject.briar.android.contact;
 import android.support.annotation.CallSuper;
 import android.support.annotation.UiThread;
 import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.text.Html;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.commonsware.cwac.anddown.AndDown;
 
 import org.briarproject.bramble.api.nullsafety.NotNullByDefault;
 import org.briarproject.bramble.util.StringUtils;
@@ -32,7 +35,11 @@ class ConversationItemViewHolder extends ViewHolder {
 		if (item.getBody() == null) {
 			text.setText("\u2026");
 		} else {
-			text.setText(StringUtils.trim(item.getBody()));
+
+			AndDown converter=new AndDown();
+			String cooked=converter.markdownToHtml(StringUtils.trim(item.getBody()));
+			CharSequence cs= Html.fromHtml(cooked);
+			text.setText(cs);
 		}
 
 		long timestamp = item.getTime();
