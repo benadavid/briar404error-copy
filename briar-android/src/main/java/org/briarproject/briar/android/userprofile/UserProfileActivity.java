5234a87bcfecbeda61baa5305687117cd38824b9
rajee10 <rvairam10@gmail.com>
2018-03-18 22:37:03 -0400

    Removed unnecessary imports

--- briar-android/src/main/java/org/briarproject/briar/android/userprofile/UserProfileActivity.java
+++ briar-android/src/main/java/org/briarproject/briar/android/userprofile/UserProfileActivity.java
@@ -12,8 +12,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Switch;
-import android.widget.ToggleButton;
 
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
