cc57848c41dca1959130f0e26d50ddb97f28c38a
benadavid <benadavid_2007@hotmail.com>
2018-03-21 16:21:40 -0400

    The test now starts from AvatarActivity in order to skip all the sign in procedures, and verifies right away whether the imageview contains the desired drawable.

--- briar-android/src/androidTest/java/org/briarproject/briar/android/splash/AvatarActivityEspressoTest.java
+++ briar-android/src/androidTest/java/org/briarproject/briar/android/splash/AvatarActivityEspressoTest.java
@@ -10,6 +10,8 @@ import android.view.ViewGroup;
 import android.view.ViewParent;
 
 import org.briarproject.briar.R;
+import org.briarproject.briar.android.navdrawer.NavDrawerActivity;
+import org.briarproject.briar.android.userprofile.AvatarActivity;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
@@ -40,206 +42,11 @@ import static org.hamcrest.Matchers.is;
 public class AvatarActivityEspressoTest {
 
 	@Rule
-	public ActivityTestRule<SplashScreenActivity> mActivityTestRule =
-			new ActivityTestRule<>(SplashScreenActivity.class);
+	public ActivityTestRule<AvatarActivity> mActivityTestRule =
+			new ActivityTestRule<>(AvatarActivity.class);
 
 	@Test
 	public void avatarActivityEspressoTest() {
-		// Added a sleep statement to match the app's execution delay.
-		// The recommended way to handle such scenarios is to use Espresso idling resources:
-		// https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		ViewInteraction appCompatButton = onView(
-				allOf(withId(R.id.btn_skip), withText("SKIP"),
-						childAtPosition(
-								childAtPosition(
-										withId(android.R.id.content),
-										0),
-								4),
-						isDisplayed()));
-		appCompatButton.perform(click());
-
-		// Added a sleep statement to match the app's execution delay.
-		// The recommended way to handle such scenarios is to use Espresso idling resources:
-		// https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		ViewInteraction textInputEditText = onView(
-				allOf(withId(R.id.nickname_entry),
-						childAtPosition(
-								childAtPosition(
-										withId(R.id.nickname_entry_wrapper),
-										0),
-								0)));
-		textInputEditText
-				.perform(scrollTo(), replaceText("David"), closeSoftKeyboard());
-
-		ViewInteraction appCompatButton2 = onView(
-				allOf(withId(R.id.next), withText("Next"),
-						childAtPosition(
-								childAtPosition(
-										withClassName(
-												is("android.widget.ScrollView")),
-										0),
-								1)));
-		appCompatButton2.perform(scrollTo(), click());
-
-		ViewInteraction textInputEditText2 = onView(
-				allOf(withId(R.id.password_entry),
-						childAtPosition(
-								childAtPosition(
-										withId(R.id.password_entry_wrapper),
-										0),
-								0)));
-		textInputEditText2.perform(scrollTo(), replaceText("david32"),
-				closeSoftKeyboard());
-
-		ViewInteraction textInputEditText3 = onView(
-				allOf(withId(R.id.password_confirm),
-						childAtPosition(
-								childAtPosition(
-										withId(R.id.password_confirm_wrapper),
-										0),
-								0)));
-		textInputEditText3.perform(scrollTo(), replaceText("david32"),
-				closeSoftKeyboard());
-
-		ViewInteraction appCompatButton3 = onView(
-				allOf(withId(R.id.next), withText("Next"),
-						childAtPosition(
-								childAtPosition(
-										withClassName(
-												is("android.widget.ScrollView")),
-										0),
-								3)));
-		appCompatButton3.perform(scrollTo(), click());
-
-		// Added a sleep statement to match the app's execution delay.
-		// The recommended way to handle such scenarios is to use Espresso idling resources:
-		// https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		ViewInteraction appCompatImageButton = onView(
-				allOf(withContentDescription("Open the navigation drawer"),
-						childAtPosition(
-								allOf(withId(R.id.toolbar),
-										childAtPosition(
-												withClassName(
-														is("android.support.design.widget.AppBarLayout")),
-												0)),
-								1),
-						isDisplayed()));
-		appCompatImageButton.perform(click());
-
-		// Added a sleep statement to match the app's execution delay.
-		// The recommended way to handle such scenarios is to use Espresso idling resources:
-		// https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-		try {
-			Thread.sleep(100);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		ViewInteraction appCompatButton4 = onView(
-				allOf(withId(R.id.showcase_button), withText("Close"),
-						childAtPosition(
-								childAtPosition(
-										withId(android.R.id.content),
-										1),
-								0),
-						isDisplayed()));
-		appCompatButton4.perform(click());
-
-		// Added a sleep statement to match the app's execution delay.
-		// The recommended way to handle such scenarios is to use Espresso idling resources:
-		// https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-		try {
-			Thread.sleep(100);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		ViewInteraction appCompatButton5 = onView(
-				allOf(withId(R.id.showcase_button), withText("Close"),
-						childAtPosition(
-								childAtPosition(
-										withId(android.R.id.content),
-										1),
-								0),
-						isDisplayed()));
-		appCompatButton5.perform(click());
-
-		// Added a sleep statement to match the app's execution delay.
-		// The recommended way to handle such scenarios is to use Espresso idling resources:
-		// https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-		try {
-			Thread.sleep(100);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		ViewInteraction appCompatButton6 = onView(
-				allOf(withId(R.id.showcase_button), withText("Close"),
-						childAtPosition(
-								childAtPosition(
-										withId(android.R.id.content),
-										1),
-								0),
-						isDisplayed()));
-		appCompatButton6.perform(click());
-
-		// Added a sleep statement to match the app's execution delay.
-		// The recommended way to handle such scenarios is to use Espresso idling resources:
-		// https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		ViewInteraction appCompatImageButton2 = onView(
-				allOf(withId(R.id.imageButton1),
-						childAtPosition(
-								allOf(withId(R.id.navigation),
-										childAtPosition(
-												withClassName(
-														is("android.widget.LinearLayout")),
-												0)),
-								1)));
-		appCompatImageButton2.perform(scrollTo(), click());
-
-		// Added a sleep statement to match the app's execution delay.
-		// The recommended way to handle such scenarios is to use Espresso idling resources:
-		// https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		ViewInteraction appCompatImageButton3 = onView(
-				allOf(withId(R.id.AvatarButton),
-						childAtPosition(
-								childAtPosition(
-										withClassName(
-												is("android.support.constraint.ConstraintLayout")),
-										0),
-								2),
-						isDisplayed()));
-		appCompatImageButton3.perform(click());
 
 		// Added a sleep statement to match the app's execution delay.
 		// The recommended way to handle such scenarios is to use Espresso idling resources:
@@ -292,6 +99,9 @@ public class AvatarActivityEspressoTest {
 						isDisplayed()));
 		imageView.check(matches(isDisplayed()));
 
+
+		//assert if imageview has the actual drawable inside
+		//main testing part
 		imageView.check(matches(hasDrawable()));
 
 		pressBack();
